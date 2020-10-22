4ba549321a9e6afb462283154f987e61b01e0a1e
Muhammad Shahbaz <rssialvi@gmail.com>
2020-10-22 23:41:06 +0500

    Customized header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Header from './Components/Header';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Header />
     </div>
   );
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,23 +1,44 @@
 import React from 'react'
-import {Nav, NavDropdown, Navbar, Form, FormControl, Button} from 'react-bootstrap'
+import { Nav, NavDropdown, Navbar, Form, FormControl, Button, Image } from 'react-bootstrap'
+import style from './header.module.css'
 
 function Header() {
     return (
         <div>
             <Navbar bg="light" expand="lg">
-                <Navbar.Brand href="#home">SS Solutions</Navbar.Brand>
+                <Navbar.Brand href="#home">
+                    <img
+                        alt=""
+                        src="/logo.png"
+                        width="30"
+                        height="30"
+                        className="d-inline-block align-top"
+                    />{' '}
+                      SS Solutions
+                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="mr-auto">
                         <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#link">Link</Nav.Link>
-                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                        <Nav.Link href="#link">About Us</Nav.Link>
+                        <NavDropdown title="Our Services" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="#action/3.1">Graphics Designing</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">Web Development</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Content Writing</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Digital Marketing</NavDropdown.Item>
                             <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.4"></NavDropdown.Item>
                         </NavDropdown>
+                        <NavDropdown title="Our Team" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="#action/3.1">Bilal Ahmad</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">Mussadiq Ali</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Irfan Ghafoor</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Muhammad Shahbaz</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="#action/3.4">Ghulam Mohayodin</NavDropdown.Item>
+                        </NavDropdown>
+                        <Nav.Link href="#link">Contact Us</Nav.Link>
+                        <Nav.Link href="#link">Help</Nav.Link>
                     </Nav>
                     <Form inline>
                         <FormControl type="text" placeholder="Search" className="mr-sm-2" />
